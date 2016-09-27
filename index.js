ef46986ebd6667f4b0c71b9f9ec28d10c9a9c981
Patrick Williams <patrickw@porch.com>
2016-09-26 17:19:30 -0700

    sanitize filenames

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
 
 var nock = require('nock');
+var sanitize = require('sanitize-filename');
 
 module.exports = function (fixtures) {
     var filenames = [];
     return {
         beforeEach: function (done) {
-            var filename = this.currentTest.title + '.json';
+            var filename = sanitize(this.currentTest.title + '.json');
             // make sure we're not reusing the nock file
             if (filenames.indexOf(filename) !== -1) {
                 return done(new Error('nock-back-mocha does not support multiple tests with the same name. ' + filename + ' cannot be reused.'));
